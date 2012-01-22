ca1433763ea1638a2150e6439297c97abd88281e
Eduard Wiebe <pusto@web.de>
2012-01-22 16:02:28 +0100

    Typo.

--- rtf.el
+++ rtf.el
@@ -381,7 +381,7 @@ properties and fresh mark is false."
   "Get maximum dimension of all charsets of coding system NAME."
   (and name
        (coding-system-p name)
-       (apply #'max (mapcar #'charset-dimenstion 
+       (apply #'max (mapcar #'charset-dimension
 			    (coding-system-charset-list name)))))
 
 (defvar rtf-default-lang nil)
